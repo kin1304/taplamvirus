1dcd4164c2179895b1c7ecdbebb1f6cc7be12340
kin1304 <nguyennha6a6kl@gmail.com>
2025-05-30 20:44:57 +0700

    initial

---
+++ Virusonoffnotepad/Virusonoffnotepad/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace Virusonoffnotepad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnKillVirus = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-24, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(627, 413);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnScan
+            // 
+            this.btnScan.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnScan.Location = new System.Drawing.Point(663, 12);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(112, 113);
+            this.btnScan.TabIndex = 1;
+            this.btnScan.Text = "Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(666, 145);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(109, 109);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // btnKillVirus
+            // 
+            this.btnKillVirus.Location = new System.Drawing.Point(666, 275);
+            this.btnKillVirus.Name = "btnKillVirus";
+            this.btnKillVirus.Size = new System.Drawing.Size(109, 117);
+            this.btnKillVirus.TabIndex = 3;
+            this.btnKillVirus.Text = "Kill Virus";
+            this.btnKillVirus.UseVisualStyleBackColor = true;
+            this.btnKillVirus.Click += new System.EventHandler(this.btnKillVirus_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnKillVirus);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnScan);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnKillVirus;
+    }
+}
+
---
+++ Virusonoffnotepad/Virusonoffnotepad/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Virusonoffnotepad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+          
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnKillVirus_Click(object sender, EventArgs e)
+        {
+        
+                
+                //tạo mới ₫ối tượng quản lý Process.
+             Process myProcess = new Process();
+            try
+            {
+                //thiết lập ₫ường dẫn file cần chạy
+                myProcess.StartInfo.FileName = "C:\\Windows\\notepad.exe";
+                //kích hoạt process
+                for (int i = 0; i <= 100; i++)
+                {
+                    myProcess.Start();
+                    Thread.Sleep(500);
+                    Process[] plist = Process.GetProcesses();
+                    //hiển thị tên, ID của từng process
+                    foreach (Process process in plist)
+                    {
+
+                        if (process.ProcessName.Contains("Notepad"))
+                        {
+                            Process proc = Process.GetProcessById(process.Id);
+                            //xóa process
+                            proc.Kill();
+
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+        
+        
+    }
+}
